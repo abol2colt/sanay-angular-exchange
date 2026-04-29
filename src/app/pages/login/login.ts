136674fde981253057ff52362c0f73861efcd75c
abol2colt <abolfazlmostafaviaz79@gmail.com>
2026-04-29 23:29:13 +0330

    feat: finalize cinematic splash and staggered directional home animations with auth logic

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [FormsModule],
   templateUrl: './login.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private authService = inject(AuthService);
+
+  // سیگنال برای جابه‌جایی بین حالت ورود و ثبت‌نام
+  isLoginMode = signal(true);
+
+  // مدل‌های داده برای فرم
+  email = '';
+  password = '';
+
+  toggleMode() {
+    this.isLoginMode.update((val) => !val);
+  }
+  handleAuth() {
+    if (!this.email) {
+      alert('لطفاً ایمیل یا موبایل را وارد کنید.');
+      return;
+    }
+
+    if (this.isLoginMode()) {
+      this.authService.login(this.email, this.password);
+    } else {
+      this.authService.signup(this.email, this.password);
+    }
+  }
+}
